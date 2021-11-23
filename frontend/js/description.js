32d32094d615b9f3f43eb77f4e15757568f46eaf
Miguel Angel Medina Martínez <mmedinam1600@gmail.com>
2021-11-22 23:19:36 -0600

    pos ya quedo

--- frontend/js/description.js
+++ frontend/js/description.js
@@ -56,11 +56,13 @@ async function getExhcnageById () {
       status = data.participants[i].status == 1 ? 'table-success' : data.participants[i].status == 2 ? 'table-danger' : 'table-secondary';
       status2 = data.participants[i].status == 1 ? 'Confirmado' : data.participants[i].status == 2 ? 'Rechazado' : 'Pendiente';
       const cardHTML = `
-        <th scope="row"><img src="./img/crab-logo.png" alt="crab logo" class="img-fluid"/></th>
+        <th scope="row">${data.participants[i].id_user}<img src="./img/crab-logo.png" alt="crab logo" class="img-fluid"/></th>
         <td>Jose Praxedes</td>
         <td>Dominguez Acosta</td>
-        <td class="${status}">${status2}</td>
         <td>josepraxedes11@gmail.com</td>
+        <td>${data.participants[i].topic || 'Sin especificar'}</td>
+        <td class="${status}">${status2}</td>
+        <td>${data.participants[i].userToGift || 'Sin definir'}</td>
         <td>
           <!--<button class="button pb-1 button-blue">
             <i class="fas fa-edit fa-sm"></i>
